e4a43498d2783a8cc385e578eaea9e0546be0f73
ALEN THOMAS VARUGHESE <85294601+alen-eng@users.noreply.github.com>
2023-04-06 01:33:06 +0530

    Add files via upload

---
+++ utils/index.tsx
@@ -0,0 +1,25 @@
+export const daysLeft = (deadline: string | number | Date) => {
+    const difference = new Date(deadline).getTime() - Date.now();
+    const remainingDays = difference / (1000 * 3600 * 24);
+  
+    return remainingDays.toFixed(0);
+  };
+  
+  export const calculateBarPercentage = (goal: any, raisedAmount: any) => {
+    const percentage = Math.round((raisedAmount * 100) / goal);
+  
+    return percentage;
+  };
+  
+  export const checkIfImage = (url: string, callback: (arg0: boolean) => void) => {
+    const img = new Image();
+    img.src = url;
+  
+    if (img.complete) callback(true);
+  
+    img.onload = () => callback(true);
+    img.onerror = () => callback(false);
+  };
+
+  export default checkIfImage;
+  
