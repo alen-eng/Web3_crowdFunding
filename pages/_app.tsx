5e406e48bde5e893dd15da087d3d88908566734c
ALEN THOMAS VARUGHESE <85294601+alen-eng@users.noreply.github.com>
2023-04-06 00:48:13 +0530

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,10 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { ThirdwebProvider } from '@thirdweb-dev/react'
 import network from './utils/network'
-import {ChainId} from "@thirdweb-dev/react";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-  <ThirdwebProvider desiredChainId={ChainId.Mumbai}>
+  <ThirdwebProvider activeChain={'mumbai'}>
     <Component {...pageProps} />
   </ThirdwebProvider>
   )
