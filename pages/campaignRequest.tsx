a04c8b9c743fb9b39594426f8c2b3b32fba92aae
ALEN THOMAS VARUGHESE <85294601+alen-eng@users.noreply.github.com>
2023-04-07 11:05:06 +0530

    Update campaignRequest.tsx

--- pages/campaignRequest.tsx
+++ pages/campaignRequest.tsx
@@ -1,5 +1,5 @@
 import React, { FormEvent, useState } from 'react';
-import { useAddress, useContract , useContractWrite } from '@thirdweb-dev/react';
+import { useAddress, useContract , } from '@thirdweb-dev/react';
 import CampaignHeader from '../components/CampaignHeader';
 import { useRouter } from 'next/router';
 type Props = {}
@@ -13,7 +13,7 @@ function addItems({}: Props) {
  process.env.NEXT_PUBLIC_CROWDFUND_COLLECTION_CONTRACT,
  "nft-collection"
   );
-  const { mutateAsync: grantRole, isLoading } = useContractWrite(contract, "grantRole")
+  
   const mintNft= async (e : FormEvent<HTMLFormElement>) => {
    e.preventDefault();
    if(!contract || !address) return;
@@ -34,7 +34,10 @@ function addItems({}: Props) {
     image : image,
    }
       try{
-          const data = await grantRole(['minter', address] as any);
+          const data = await contract.roles.grant(
+          "minter",
+          address,
+        );
           const tx=await contract.mintTo(address, metadata);
           const receipt= tx.receipt;
           const tokenId = tx.id;
