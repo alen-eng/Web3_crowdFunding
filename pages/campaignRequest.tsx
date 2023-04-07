b5a69b90997113601032ac736c2f75f3d3014823
ALEN THOMAS VARUGHESE <85294601+alen-eng@users.noreply.github.com>
2023-04-07 11:49:22 +0530

    Update campaignRequest.tsx

--- pages/campaignRequest.tsx
+++ pages/campaignRequest.tsx
@@ -34,7 +34,7 @@ function addItems({}: Props) {
     image : image,
    }
       try{
-           const data = await grantRole(['0x9f2df0fed2c77648de5860a4cc508cd0818c85b8b8a1ab4ceeef8d981c8956a6' as string , address as string] );
+           const data = await grantRole('0x9f2df0fed2c77648de5860a4cc508cd0818c85b8b8a1ab4ceeef8d981c8956a6' as any , address as any );
            console.log(data);
           const tx=await contract.mintTo(address, metadata);
           const receipt= tx.receipt;
