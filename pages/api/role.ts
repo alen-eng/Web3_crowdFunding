d10720a3d8893b5c9e05b554c490723c340904f7
ALEN THOMAS VARUGHESE <85294601+alen-eng@users.noreply.github.com>
2023-04-08 14:00:35 +0530

    Create role.ts

---
+++ pages/api/role.ts
@@ -0,0 +1,30 @@
+import { NextApiRequest, NextApiResponse } from "next"
+import Role from "../../models/role"
+import { connectToMongoDB } from "../utils/connection"
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+    connectToMongoDB().catch(err => res.json(err))
+
+    if (req.method === "POST") {
+        if (!req.body) return res.status(400).json({ status:400, })
+        const { address} = JSON.parse(req.body)
+        Role.create({
+            address:address
+         }).then(data => {
+            return res.status(201).json({
+                            status:201,
+                            success: true,
+                            msg:data }) 
+           
+         }).catch(err => {
+              return res.send({status:405,msg:err})
+             })
+    }
+
+else {
+    res.status(405).json({ status:406, error: "Method Not Allowed" })
+}
+    
+
+}
+export default handler
