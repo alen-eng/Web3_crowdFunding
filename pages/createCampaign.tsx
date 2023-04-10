7c7be487350433980c527f2ec61a5e8431c522df
ALEN THOMAS VARUGHESE <85294601+alen-eng@users.noreply.github.com>
2023-04-10 18:04:42 +0530

    Update createCampaign.tsx

--- pages/createCampaign.tsx
+++ pages/createCampaign.tsx
@@ -64,7 +64,7 @@ const target= e.target as typeof e.target & {
      target.elements.Description.value,
       target.elements.Target.value,
         new Date(target.elements.Deadline.value).getTime(),
-        selectedNFT.metadata.image],
+        selectedNFT.metadata.image] as any,
         {
             onSuccess(data, variables, context) {
                 console.log("Success : ", data, variables, context);
