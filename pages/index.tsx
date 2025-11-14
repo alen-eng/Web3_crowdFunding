b958cd5331e51643e9ba4b1f13b41423a14272ed
ALEN THOMAS VARUGHESE <85294601+alen-eng@users.noreply.github.com>
2025-11-14 22:52:45 +0100

    Remove width and height from Image component
    Removed width and height props from Image component.

--- pages/index.tsx
+++ pages/index.tsx
@@ -41,8 +41,6 @@ const router= useRouter()
            <Image
               src={imageslide[currentslide].url}
               alt='Bgimage'
-              {/* width={1080}
-              height={10} */}
 			  placeholder='blur'
 			  sizes='(max-width: 768px) 100vw, (max-width: 1080px) 50vw, 33vw'
               onClick={() => currentslide + 1 < 3 ? setcurrentslide(currentslide + 1) : setcurrentslide(0)}
